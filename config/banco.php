8d8a8e06d0b124e95cfdfef170cc6ec88362de20
Otavio Faccioli <otavio.faccioli@gmail.com>
2022-03-18 15:44:44 -0300

    Add files via upload

---
+++ config/banco.php
@@ -0,0 +1,15 @@
+﻿<?php
+
+$local = "localhost";
+
+$usuario = "root";
+
+$senha = "";
+
+$banco = "osgh";
+
+$conexao = mysqli_connect( $local, $usuario, $senha, $banco);
+
+//$base = mysqli_select_db($banco,$conexao);
+
+?>
